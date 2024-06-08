eeb7e2dc1d340bee6cd227e2307284bca3d7110e
Kunal Garg <kunalgarg054@gmail.com>
2024-06-08 19:20:36 +0530

    feat: tabs feat added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,10 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Skeleton } from "@/components/ui/skeleton";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip"
-import { Layers2 } from 'lucide-react';
-
-interface Transaction {
-  _id: string;
-  status: string;
-  transactionHash: string;
-  type: string;
-  blockNumber: number;
-  age: string;
-}
+import { useState } from 'react';
+import All from './components/tabs/all';
+import Deploy from './components/tabs/deploy';
+import DeployAccount from './components/tabs/deployAccount';
+import Invoke from './components/tabs/invoke';
+import Declare from './components/tabs/declare';
+import L1_Handler from './components/tabs/l1Handler';
 
 const tabs = [
   { name: 'All', id: 'all' },
@@ -35,31 +15,11 @@ const tabs = [
   { name: 'l1_handler', id: 'l1_handler' },
 ];
 
-const formatHash = (hash: string) => {
-  return `${hash.slice(0, 6)}...${hash.slice(-4)}`;
-};
-
 const App: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>('all');
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchTransactions = async () => {
-      try {
-        const { data } = await axios.get<Transaction[]>('http://localhost:3000/transactions');
-        setTransactions(data);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching transactions', error);
-      }
-    };
-
-    fetchTransactions();
-  }, []);
 
   return (
-    <main className={`bg-[#121212] p-9 text-white ${loading ? 'h-screen' : 'h-full'}`}>
+    <main className={`bg-[#121212] p-9 text-white h-full`}>
       <div className="bg-[rgb(27,27,27)] h-max rounded-xl p-7">
         <div className="grid gap-y-1">
           <div className="text-2xl">
@@ -86,69 +46,12 @@ const App: React.FC = () => {
         </div>
 
         <div className='my-6'>
-          <Table>
-            <TableHeader>
-              <TableRow className='hover:bg-transparent'>
-                <TableHead className="w-[100px]">Status</TableHead>
-                <TableHead>Hash</TableHead>
-                <TableHead>Type</TableHead>
-                <TableHead>Block</TableHead>
-                <TableHead className="text-right">Age</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {loading && (
-                <TableRow>
-                  <TableCell>
-                    <Skeleton className="bg-slate-500 w-[50px] h-[20px]" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton className="bg-slate-500 w-[100px] h-[20px]" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton className="bg-slate-500 w-[100px] h-[20px]" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton className="bg-slate-500 w-[100px] h-[20px]" />
-                  </TableCell>
-                  <TableCell className="flex justify-end">
-                    <Skeleton className="bg-slate-500 w-[50px] h-[20px]" />
-                  </TableCell>
-                </TableRow>            
-              )}
-              {!loading && activeTab === 'all' && transactions.map((transaction) => (
-                <TableRow key={transaction._id}>
-                  <TableCell>
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger>
-                          <Layers2 size={16} fill="green" strokeWidth={1}/>
-                        </TooltipTrigger>
-                        <TooltipContent className='bg-white text-black'>
-                          <p>{transaction.status}</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </TableCell>
-                  <TableCell>
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger>{formatHash(transaction.transactionHash)}</TooltipTrigger>
-                        <TooltipContent className='bg-white text-black'>
-                          <p >{transaction.transactionHash}</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </TableCell>
-                  <TableCell>
-                    {transaction.type}
-                  </TableCell>
-                  <TableCell>{transaction.blockNumber}</TableCell>
-                  <TableCell className="text-right">{transaction.age}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          {activeTab === 'all' && <All />}
+          {activeTab === 'declare' && <Declare />}
+          {activeTab === 'deploy' && <Deploy />}
+          {activeTab === 'deploy_account' && <DeployAccount />}
+          {activeTab === 'invoke' && <Invoke />}
+          {activeTab === 'l1_handler' && <L1_Handler />}
         </div>
       </div>
     </main>
---
+++ frontend/src/components/tabs/all.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
+  import { Skeleton } from "@/components/ui/skeleton";
+  import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+  import { Layers2 } from 'lucide-react';
+
+  interface Transaction {
+    _id: string;
+    status: string;
+    transactionHash: string;
+    type: string;
+    blockNumber: number;
+    age: string;
+  }
+
+    const formatHash = (hash: string) => {
+        return `${hash.slice(0, 6)}...${hash.slice(-4)}`;
+    };
+
+const All: React.FC = () => {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchTransactions = async () => {
+          try {
+            const { data } = await axios.get<Transaction[]>('https://karnot-zeta.vercel.app/transactions');
+            setTransactions(data);
+            setLoading(false);
+          } catch (error) {
+            console.error('Error fetching transactions', error);
+          }
+        };
+    
+        fetchTransactions();
+      }, []);
+
+  return (
+    <div>
+        <Table>
+            <TableHeader>
+              <TableRow className='hover:bg-transparent'>
+                <TableHead className="w-[100px]">Status</TableHead>
+                <TableHead>Hash</TableHead>
+                <TableHead>Type</TableHead>
+                <TableHead>Block</TableHead>
+                <TableHead className="text-right">Age</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {loading && (
+                <TableRow>
+                  <TableCell>
+                    <Skeleton className="bg-slate-500 w-[50px] h-[20px]" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="bg-slate-500 w-[100px] h-[20px]" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="bg-slate-500 w-[100px] h-[20px]" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="bg-slate-500 w-[100px] h-[20px]" />
+                  </TableCell>
+                  <TableCell className="flex justify-end">
+                    <Skeleton className="bg-slate-500 w-[50px] h-[20px]" />
+                  </TableCell>
+                </TableRow>            
+              )}
+              {!loading && transactions.map((transaction) => (
+                <TableRow key={transaction._id}>
+                  <TableCell>
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Layers2 size={16} fill="green" strokeWidth={1}/>
+                        </TooltipTrigger>
+                        <TooltipContent className='bg-white text-black'>
+                          <p>{transaction.status}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </TableCell>
+                  <TableCell>
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>{formatHash(transaction.transactionHash)}</TooltipTrigger>
+                        <TooltipContent className='bg-white text-black'>
+                          <p >{transaction.transactionHash}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </TableCell>
+                  <TableCell>
+                    {transaction.type}
+                  </TableCell>
+                  <TableCell>{transaction.blockNumber}</TableCell>
+                  <TableCell className="text-right">{transaction.age}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+    </div>
+  )
+}
+
+export default All
